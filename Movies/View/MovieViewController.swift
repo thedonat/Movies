57b1e4841ac21fc5bc0a52df3e560cd626d12713
Burak Donat <burakdonat@gmail.com>
2020-09-26 11:18:44 +0300

    create project directory

---
+++ Movies/View/MovieViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MovieViewController.swift
+//  Movies
+//
+//  Created by Burak Donat on 26.09.2020.
+//  Copyright © 2020 Burak Donat. All rights reserved.
+//
+
+import UIKit
+
+class MovieViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
