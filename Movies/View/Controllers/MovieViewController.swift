a13be15fe25bbf1943146fbc6b1d9ba946ed680a
Burak Donat <burakdonat@gmail.com>
2020-09-26 12:02:19 +0300

    make UI connections

--- Movies/View/Controllers/MovieViewController.swift
+++ Movies/View/Controllers/MovieViewController.swift
@@ -10,8 +10,36 @@ import UIKit
 
 class MovieViewController: UIViewController {
 
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var movieTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+    }
+    
+    @IBAction func indexDidChange(_ sender: UISegmentedControl) {
+        switch segmentedControl.selectedSegmentIndex {
+        case 0:
+            print(0)
+        case 1:
+            print(1)
+        case 2:
+            print(2)
+        default:
+            break;
+        }
+    }
+}
+
+
+extension MovieViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = movieTableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! CustomCell
+        return cell
     }
 }
--- Movies/View/Controllers/TvShowViewController.swift
+++ Movies/View/Controllers/TvShowViewController.swift
@@ -10,9 +10,34 @@ import UIKit
 
 class TvShowViewController: UIViewController {
 
+    @IBOutlet weak var tvShowTableView: UITableView!
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func indexDidChange(_ sender: UISegmentedControl) {
+        switch segmentedControl.selectedSegmentIndex {
+        case 0:
+            print(0)
+        case 1:
+            print(1)
+        default:
+            break;
+        }
+    }
+}
+
+extension TvShowViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tvShowTableView.dequeueReusableCell(withIdentifier: "TvCell", for: indexPath) as! CustomCell
+        return cell
     }
 }
