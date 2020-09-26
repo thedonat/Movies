13c5d50b81c0ebf0ce62c529865c6eba039cf8b6
Burak Donat <burakdonat@gmail.com>
2020-09-26 11:47:47 +0300

    create base UI components

---
+++ Movies/View/Cells/CustomCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomCell.swift
+//  Movies
+//
+//  Created by Burak Donat on 26.09.2020.
+//  Copyright © 2020 Burak Donat. All rights reserved.
+//
+
+import UIKit
+
+class CustomCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Movies/View/Controllers/TvShowViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TvShowViewController.swift
+//  Movies
+//
+//  Created by Burak Donat on 26.09.2020.
+//  Copyright © 2020 Burak Donat. All rights reserved.
+//
+
+import UIKit
+
+class TvShowViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
